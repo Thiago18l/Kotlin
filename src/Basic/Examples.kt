30bc7b0e3d92a620278aca98794723f6223379e0
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-06-11 16:29:37 -0300

    feat: String interpolation and type check

--- src/Basic/Examples.kt
+++ src/Basic/Examples.kt
@@ -11,4 +11,18 @@ fun double (x: Int): Int = x * 2
 fun main(args: Array<String>) {
     var result = double(2)
     println(result)
+
+    println(s)
+
+    var a: String = "abc"
+    // a = null  error!
+
+    var b: String? = "abc"
+    b = null
+    println(b)
 }
+
+// String interpolation
+
+val num = 10
+val s = "i $num"
