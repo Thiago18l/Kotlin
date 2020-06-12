f010da28d34816b7a19bc475c65289c0fdc71a03
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-06-12 17:57:02 -0300

    feat: enums with abstract item

--- src/Basic/Enums.kt
+++ src/Basic/Enums.kt
@@ -5,3 +5,19 @@ enum class Color (val rgb: Int) {
     GREEN(0x00FF00),
     BLUE(0x0000FF)
 }
+
+enum class ColorFun {
+    RED {
+        override val rgb: Int = 0xFF0000
+    },
+    GREE {
+        override val rgb: Int = 0x00FF00
+    },
+    BLUE {
+        override val rgb: Int = 0x0000FF
+    }
+    ;
+    abstract val rgb: Int
+
+    fun colorString() = "#%06X".format(0xFFFFFF and rgb)
+}
